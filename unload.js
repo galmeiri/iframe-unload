c963136abdd67753d2150ea9bc58d527e51264a2
galmeiri <galmeiri11@gmail.com>
2020-09-21 20:01:17 +0300

    Update unload.js

--- unload.js
+++ unload.js
@@ -1,3 +1,16 @@
-window.addEventListener('unload', () => {
-	fetch("https://blahblah.com/blah");
-})
+window.addEventListener("unload", ()=>{
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', '/galmeirigalmeirigalmeiri');
+
+        xhr.responseType = 'json';
+
+        xhr.send();
+
+        // the response is {"message": "Hello, world!"}
+        xhr.onload = function() {
+          let responseObj = xhr.response;
+          //alert(responseObj.message); // Hello, world!
+        };
+
+    });
